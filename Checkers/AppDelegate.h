4e740f9c6080054613c9f1002f4d8ac8255f219e
Dylan Shackelford <djshackx@gmail.com>
2016-06-14 21:56:28 -0700

    Initial Commit

---
+++ Checkers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Checkers
+//
+//  Created by Dylan Shackelford on 6/14/16.
+//  Copyright © 2016 Dylan Shackelford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Checkers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Checkers
+//
+//  Created by Dylan Shackelford on 6/14/16.
+//  Copyright © 2016 Dylan Shackelford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
