a84bd4a8e425d94bcd06ea18e771eebe517ae033
Dylan Shackelford <djshackx@gmail.com>
2016-06-27 16:41:14 -0700

    added some class files for computer movement

---
+++ Checkers/ComputerMover.h
@@ -0,0 +1,16 @@
+//
+//  ComputerMover.h
+//  Checkers
+//
+//  Created by Dylan Shackelford on 6/27/16.
+//  Copyright © 2016 Dylan Shackelford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComputerMover : NSObject
+{
+    double turn;
+}
+
+@end
---
+++ Checkers/ComputerMover.m
@@ -0,0 +1,16 @@
+//
+//  ComputerMover.m
+//  Checkers
+//
+//  Created by Dylan Shackelford on 6/27/16.
+//  Copyright © 2016 Dylan Shackelford. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ComputerMover.h"
+
+@implementation ComputerMover
+
+
+
+@end
